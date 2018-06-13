49dab1a22fe67b5b940fdbc941e0059c4cea5f39
Kain <904616537@qq.com>
2018-06-13 23:09:27 +0800

    _

--- src/views/index.vue
+++ src/views/index.vue
@@ -7,7 +7,16 @@
 	    <router-link to="/regist">
 	        <div class="pub-btn">{{ $t("home.regist") }}</div>
 	    </router-link>
+		<div class="pub-btn" @click="show = true">{{ $t("home.shop") }}</div>
+		
 	    <v-lang></v-lang>
+	    <div class="keep" v-if="show">
+	    	<div class="block">
+		    	<img src="static/imgs/wechatcode.jpeg">
+		    	<h3>{{ $t("home.shop_text") }}</h3>
+		    	<div @click="show = false" class="close">{{ $t("home.close") }}</div>
+	    	</div>
+		</div>
 	</div>
 </template>
 <script> 
@@ -18,8 +27,8 @@
 		name : 'home',
 		data() {
 			return {
-				
+				show : false
 			}
-		},
+		}
 	}
 </script>
